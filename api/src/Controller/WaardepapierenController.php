50b6e61dfcc73cd571db1c9d30d2bcc20c1bff03
Wilco Louwerse <wilco@conduction.nl>
2020-11-24 16:38:52 +0100

    WIP, added download function to waardepapierenController

--- api/src/Controller/WaardepapierenController.php
+++ api/src/Controller/WaardepapierenController.php
@@ -49,4 +49,54 @@ class WaardepapierenController extends AbstractController
 
         return $variables;
     }
+
+    /**
+     * This function will prompt a downloaden for the qr code.
+     *
+     * It provides the following optional query parameters
+     * size: the size of the image renderd, default  300
+     * margin: the maring on the image in pixels, default 10
+     * file: the file type renderd, default png
+     * encoding: the encoding used for the file, default: UTF-8
+     *
+     * @Route("/download/{id}")
+     */
+    public function downloadAction(Session $session, $id, $type = 'png', Request $request, FlashBagInterface $flash, CommonGroundService $commonGroundService, ApplicationService $applicationService, ParameterBagInterface $params, QrCodeFactoryInterface $qrCodeFactory)
+    {
+        $splits = explode('.', $id);
+        $id = $splits[0];
+        $extention = $splits[1];
+        $certificate = $commonGroundService->getResource(['component' => 'waar', 'type' => 'certificates', 'id'=>$id]);
+
+//        $url = $this->generateUrl('app_chin_checkin', ['code'=>$node['reference']], UrlGeneratorInterface::ABSOLUTE_URL);
+//
+//        $configuration = $node['qrConfig'];
+//        if ($request->query->get('size')) {
+//            $configuration['size'] = $request->query->get('size', 300);
+//        }
+//        if ($request->query->get('margin')) {
+//            $configuration['margin'] = $request->query->get('margin', 10);
+//        }
+//
+//        $qrCode = $qrCodeFactory->create($url, $configuration);
+//
+//        // Set advanced options
+//        $qrCode->setWriterByName($request->query->get('file', $extention));
+//        $qrCode->setEncoding($request->query->get('encoding', 'UTF-8'));
+//        //$qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::HIGH());
+//
+//        $filename = 'qr-code.'.$extention;
+//
+//        $response = new Response($qrCode->writeString());
+//        // Create the disposition of the file
+//        $disposition = $response->headers->makeDisposition(
+//            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+//            $filename
+//        );
+//
+//        // Set the content disposition
+//        $response->headers->set('Content-Disposition', $disposition);
+//
+//        return $response;
+    }
 }
