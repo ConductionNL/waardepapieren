b2a41734599a726eeefd48b63933bb949bc4c4a5
Barry Brands <barrybrands02@hotmail.com>
2021-06-08 15:14:42 +0200

    Code added for hashing

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -12,6 +12,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\UrlHelper;
+use Symfony\Component\HttpFoundation\Session\Session;
+
 /**
  * The Procces test handles any calls that have not been picked up by another test, and wel try to handle the slug based against the wrc.
  *
@@ -30,20 +33,18 @@ class DefaultController extends AbstractController
         // On an index route we might want to filter based on user input
         $variables['query'] = array_merge($request->query->all(), $variables['post'] = $request->request->all());
 
-        if ($this->getUser() && $request->query->get('status') && $request->query->get('type')) {
-            $variables['certificates'] = $commonGroundService->getResourceList(['component' => 'wari', 'type' => 'certificates'], ['person' => $this->getUser()->getPerson()])['hydra:member'];
-            $variables['certificates'][] = array('type' => 'geboorte akte', 'created' => '17-09-2020', 'id' => '1');
-
-            $variables['certificate']['type'] = $request->query->get('type');
-            $variables['certificate']['organization'] = '001516814';
-            $variables['certificate']['person'] = $this->getUser()->getPerson();
-
-            $variables['certificate'] = $commonGroundService->createResource($variables['certificate'], ['component' => 'waar', 'type' => 'certificates']);
-
-            return $variables;
-        }
-
-        $variables['uid'] = Uuid::uuid4();
+//        if ($this->getUser() && $request->query->get('status') && $request->query->get('type')) {
+//            $variables['certificates'] = $commonGroundService->getResourceList(['component' => 'wari', 'type' => 'certificates'], ['person' => $this->getUser()->getPerson()])['hydra:member'];
+//            $variables['certificates'][] = array('type' => 'geboorte akte', 'created' => '17-09-2020', 'id' => '1');
+//
+//            $variables['certificate']['type'] = $request->query->get('type');
+//            $variables['certificate']['organization'] = '001516814';
+//            $variables['certificate']['person'] = $this->getUser()->getPerson();
+//
+//            $variables['certificate'] = $commonGroundService->createResource($variables['certificate'], ['component' => 'waar', 'type' => 'certificates']);
+//
+//            return $variables;
+//        }
 
         $variables['types'][] = [
             'name' => 'Akte van geboorte',
@@ -78,52 +79,149 @@ class DefaultController extends AbstractController
             $variables['certificates'][] = array('type' => 'geboorte akte', 'created' => '17-09-2020', 'id' => '1');
         }
 
-        if ($request->isMethod('POST')) {
-            $variables['certificate'] = $request->request->all();
-            $typeinfo = json_decode($variables['certificate']['typeinfo']);
-            $variables['certificate']['type'] = $typeinfo->type;
-            $variables['certificate']['organization'] = '001516814';
+        if ($this->getUser() && $request->isMethod('POST')) {
+            $variables['values'] = $request->request->all();
+//            $typeinfo = json_decode($variables['values']);
+
+
+//            $variables['certificate']['type'] = $typeinfo->type;
+//            $variables['certificate']['organization'] = '001516814';
 //            $variables['certificate']['person'] = 'testpersoonbarry';
 
 //            Re enable
 //            $variables['certificate'] = $commonGroundService->createResource($variables['certificate'], ['component' => 'waar', 'type' => 'certificates']);
 
 //            $variables['certificate']['claim'] = base64_encode(json_encode($variables['certificate']['claim']));
-//            if (isset($typeinfo->price)) {
-//                $variables['certificate']['price'] = $typeinfo->price;
-//                Guzzle call to ... ?
-//                $headers = [
-//                    'Accept'        => 'application/ld+json',
-//                    'Content-Type'  => 'application/json',
-//                    'PSPID' => 'gemhoorn',
-//                    'shaSignature' => 'ZabNz@ASFrZy5Hg6',
-//                    'signatureHashAlgorithm' => 'Sha256',
-//                    'signatureMethod' => 'AllParameters'
-//                ];
-//
-//                $client = new Client([
-//                    'headers'  => $headers,
-//                    'timeout'  => 30.0,
-//                ]);
-//
-//                $body = [
-//                    'price' => $variables['certificate']['price']
-//                ];
-//
-//                $response = $client->request('POST', 'https://secure.ogone.com/ncol/test/orderstandard.asp', [
-//                    'json'         => $body,
-//                ]);
-//
-//                $response = json_decode($response->getBody()->getContents(), true);
-//
-//                var_dump($response);die;
-//
-//                header("Location: " + $response['paymentUrl?']);
-//                exit;
-//            }
+
         }
 
 //        $variables['claim'] = base64_encode(json_encode(array("Peter"=>35, "Ben"=>37, "Joe"=>43)));
+
+        return $variables;
+    }
+
+    /**
+     * @Route("/pay-certificate")
+     * @Template
+     */
+    public function payCertificateAction
+    (
+        CommonGroundService $commonGroundService,
+        Request $request,
+        ParameterBagInterface $params,
+        UrlHelper $urlHelper,
+        Session $session
+    )
+    {
+        $variables = [];
+
+        $shaSignature = $params->get('app_shasign');
+
+        if (isset($shaSignature) && $request->query->get('orderID') && $request->query->get('PAYID') && $request->query->get('SHASIGN') && $this->getUser()) {
+            $variables['paramsArray'] = $request->query->all();
+
+            $keyArray = [];
+            foreach ($variables['paramsArray'] as $key => $value) {
+                // Dont take the hashed shasign
+                if ($key !== 'SHASIGN') {
+                    $keyArray[strtoupper($key)] = $value;
+                }
+            }
+            ksort($keyArray);
+
+            $signature = [];
+            foreach ($keyArray as $key => $value) {
+                $signature[] = $key . "=" . $value . $shaSignature;
+            }
+            $hashedSign = strtoupper(hash('sha256', implode('', $signature)));
+
+            if ($hashedSign === $request->query->get('SHASIGN')) {
+                $variables['hashResult'] = 'success';
+            } else {
+                $variables['hashResult'] = 'failed';
+            }
+
+            if (isset($variables['paramsArray']['STATUS']) && ($variables['paramsArray']['STATUS'] == '5' ||
+                    $variables['paramsArray']['STATUS'] == '9' || $variables['paramsArray']['STATUS'] == '51' ||
+                    $variables['paramsArray']['STATUS'] == '91')) {
+
+//                Create certificate if type is in session
+                if ($session->get('type')) {
+                    $variables['certificate']['type'] = $session->get('type');
+                    $variables['certificate']['organization'] = '001516814';
+                    $variables['certificate']['person'] = $this->getUser()->getPerson();
+                    $variables['certificate'] = $commonGroundService->createResource($variables['certificate'], ['component' => 'waar', 'type' => 'certificates']);
+                    $variables['certificate']['claim'] = base64_encode(json_encode($variables['certificate']['claim']));
+
+                    $variables['certificates'] = $commonGroundService->getResourceList(['component' => 'wari', 'type' => 'certificates'], ['person' => $this->getUser()->getPerson()])['hydra:member'];
+                }
+
+            }
+
+//            var_dump($hashedSign);
+//            var_dump($request->query->get('SHASIGN'));
+//            die;
+        } else if (isset($shaSignature) && $request->isMethod('POST') && $this->getUser()) {
+            $variables['values'] = $request->request->all();
+            $typeinfo = json_decode($variables['values']['typeinfo']);
+
+            if (isset($typeinfo)) {
+                $session->set('type', $typeinfo->type);
+            }
+
+            $variables['paymentArray'] = [];
+            $variables['paymentArray'] = [
+                'PSPID' => 'gemhoorn',
+                'orderid' => (string)Uuid::uuid4(),
+                'amount' => $typeinfo->price * 100,
+                'currency' => 'EUR',
+                'language' => 'nl_NL',
+                'CN' => $this->getUser()->getName(),
+                'EMAIL' => 'barry@conduction.nl',
+                'TITLE' => 'Certificate',
+                'BGCOLOR' => 'white',
+                'TXTCOLOR' => 'black',
+                'TBLBGCOLOR' => 'white',
+                'TBLTXTCOLOR' => 'black',
+                'BUTTONBGCOLOR' => 'white',
+                'BUTTONTXTCOLOR' => 'black',
+                'FONTTYPE' => 'Verdana',
+                'ACCEPTURL' => $urlHelper->getAbsoluteUrl('/pay-certificate'),
+                'EXCEPTIONURL' => $urlHelper->getAbsoluteUrl('/pay-certificate'),
+                'DECLINEURL' => $urlHelper->getAbsoluteUrl('/pay-certificate'),
+                'CANCELURL' => $urlHelper->getAbsoluteUrl('/pay-certificate')
+            ];
+            $variables['keyArray'] = [];
+
+            foreach ($variables['paymentArray'] as $key => $value) {
+                $variables['keyArray'][strtoupper($key)] = $value;
+            }
+
+            ksort($variables['keyArray']);
+//            var_dump($variables['keyArray']);
+            $variables['signature'] = [];
+
+            foreach ($variables['keyArray'] as $key => $value) {
+                $variables['signature'][] = $key . "=" . $value . $shaSignature;
+            }
+//            var_dump(implode('?', $variables['signature']));
+//            var_dump($variables['signature']);
+
+            $variables['paymentArray']['SHASign'] = hash('sha256', implode('', $variables['signature']));
+            /*            var_dump($variables['paymentArray']['SHASign']);
+                        die;*/
+            $variables['status'] = 'test';
+
+//            $variables['certificate']['type'] = $typeinfo->type;
+//            $variables['certificate']['organization'] = '001516814';
+//            $variables['certificate']['person'] = 'testpersoonbarry';
+
+//            Re enable
+//            $variables['certificate'] = $commonGroundService->createResource($variables['certificate'], ['component' => 'waar', 'type' => 'certificates']);
+
+//            $variables['certificate']['claim'] = base64_encode(json_encode($variables['certificate']['claim']));
+
+        }
 
         return $variables;
     }
