a9db06479e85208f952ac56f5d8abe7210a7a70c
Robert Zondervan <Robert@conduction.nl>
2021-08-09 15:30:41 +0200

    First step to do SAML in an authenticator

--- api/src/Security/CommongroundDigidAuthenticator.php
+++ api/src/Security/CommongroundDigidAuthenticator.php
@@ -12,6 +12,8 @@ namespace App\Security;
 use Conduction\CommonGroundBundle\Security\User\CommongroundUser;
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Client;
+use OneLogin\Saml2\Auth;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,6 +26,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
 
 class CommongroundDigidAuthenticator extends AbstractGuardAuthenticator
 {
@@ -34,8 +37,10 @@ class CommongroundDigidAuthenticator extends AbstractGuardAuthenticator
     private $router;
     private $urlGenerator;
     private $session;
+    private Auth $samlAuth;
+    private XmlEncoder $xmlEncoder;
 
-    public function __construct(EntityManagerInterface $em, ParameterBagInterface $params, CommonGroundService $commonGroundService, CsrfTokenManagerInterface $csrfTokenManager, RouterInterface $router, UrlGeneratorInterface $urlGenerator, SessionInterface $session)
+    public function __construct(EntityManagerInterface $em, ParameterBagInterface $params, CommonGroundService $commonGroundService, CsrfTokenManagerInterface $csrfTokenManager, RouterInterface $router, UrlGeneratorInterface $urlGenerator, SessionInterface $session, Auth $samlAuth, XmlEncoder $xmlEncoder)
     {
         $this->em = $em;
         $this->params = $params;
@@ -44,6 +49,8 @@ class CommongroundDigidAuthenticator extends AbstractGuardAuthenticator
         $this->router = $router;
         $this->urlGenerator = $urlGenerator;
         $this->session = $session;
+        $this->samlAuth = $samlAuth;
+        $this->xmlEncoder = $xmlEncoder;
     }
 
     /**
@@ -54,7 +61,7 @@ class CommongroundDigidAuthenticator extends AbstractGuardAuthenticator
     public function supports(Request $request)
     {
         return 'app_default_samltest' === $request->attributes->get('_route')
-            && $request->isMethod('POST');
+            && $request->isMethod('GET');
     }
 
     /**
@@ -64,18 +71,75 @@ class CommongroundDigidAuthenticator extends AbstractGuardAuthenticator
     public function getCredentials(Request $request)
     {
         $credentials = [
-            'bsn'   => $request->request->get('bsn'),
+            'SAMLart'   => $request->query->get('SAMLart'),
 
         ];
 
         return $credentials;
     }
 
+    public function samlartToBsn(string $artifact): string
+    {
+        $date = date('Y-m-d\TH:i:s\Z');
+        $config = $this->samlAuth->getSettings()->getSPData();
+
+        $xml = '
+                <samlp:ArtifactResolve
+        xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
+        xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
+        xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
+        xmlns:ec="http://www.w3.org/2001/10/xml-exc-c14n#"
+        ID="_1330416073" Version="2.0" IssueInstant="'.$date.'">
+        <saml:Issuer>'.$config['entityId'].'</saml:Issuer>
+        <samlp:Artifact>'.$artifact.'</samlp:Artifact>
+        </samlp:ArtifactResolve>';
+
+        $signed = \OneLogin\Saml2\Utils::addSign($xml, $config['privateKey'], $config['x509cert']);
+        $signed = str_replace('<?xml version="1.0"?>', '', $signed);
+
+        $soap = '
+        <soapenv:Envelope
+        xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <soapenv:Body>
+        '.$signed.'
+        </soapenv:Body>
+        </soapenv:Envelope>';
+
+
+        $client = new Client([
+            'base_uri' => 'https://was-preprod1.digid.nl',
+            'timeout'  => 5.0,
+            'ssl_key'  => $this->params->get('app_ssl_key'),
+            'cert'     => $this->params->get('app_certificate'),
+        ]);
+
+        $response = $client->request('POST', '/saml/idp/resolve_artifact', [
+            'headers' => [
+                'Content-Type' => 'text/xml',
+                'SOAPAction'   => $config['entityId'],
+            ],
+            'body' => $soap,
+        ]);
+
+        $result = $response->getBody()->getContents();
+
+        $data = $this->xmlEncoder->decode($result, 'xml');
+
+        $nameId = $data['soapenv:Body']['samlp:ArtifactResponse']['samlp:Response']['saml:Assertion']['saml:Subject']['saml:NameID'];
+        $nameIdExplode = explode(":", $nameId);
+
+        return end($nameIdExplode);
+    }
+
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
+        $bsn = $this->samlartToBsn($credentials['samlArt']);
+
         // Aan de hand van BSN persoon ophalen uit haal centraal
         try {
-            $user = $this->commonGroundService->getResource(['component' => 'brp', 'type' => 'ingeschrevenpersonen', 'id' => $credentials['bsn']]);
+            $user = $this->commonGroundService->getResource(['component' => 'brp', 'type' => 'ingeschrevenpersonen', 'id' => $bsn]);
         } catch (\Throwable $e) {
             return;
         }
@@ -100,11 +164,11 @@ class CommongroundDigidAuthenticator extends AbstractGuardAuthenticator
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        try {
-            $user = $this->commonGroundService->getResource(['component' => 'brp', 'type' => 'ingeschrevenpersonen', 'id' => $credentials['bsn']]);
-        } catch (\Throwable $e) {
-            return;
-        }
+//        try {
+//            $user = $this->commonGroundService->getResource(['component' => 'brp', 'type' => 'ingeschrevenpersonen', 'id' => $credentials['bsn']]);
+//        } catch (\Throwable $e) {
+//            return;
+//        }
 
         // no adtional credential check is needed in this case so return true to cause authentication success
         return true;
@@ -112,22 +176,18 @@ class CommongroundDigidAuthenticator extends AbstractGuardAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        $backUrl = $request->request->get('back_url');
+//        $backUrl = $request->request->get('back_url');
         $bsn = $request->request->get('bsn');
         $user = $this->commonGroundService->getResource(['component' => 'brp', 'type' => 'ingeschrevenpersonen', 'id' => $bsn]);
 
         $this->session->set('user', $user);
 
-        return new RedirectResponse($backUrl);
+        return new RedirectResponse($this->urlGenerator->generate('app_default_index'));
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        if ($this->params->get('app_subpath') && $this->params->get('app_subpath') != 'false') {
-            return new RedirectResponse('/'.$this->params->get('app_subpath').$this->router->generate('app_user_digispoof', []));
-        }
-
-        return new RedirectResponse($this->router->generate('app_user_digispoof', ['response' => $request->request->get('back_url'), 'back_url' => $request->request->get('back_url')]));
+        return new RedirectResponse($this->params->get('app_url').'/saml/Login');
     }
 
     /**
