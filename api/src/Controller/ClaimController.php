40b8f6f1723aff374d96397821bbf773ca6882de
Robert Zondervan <robert@conduction.nl>
2021-10-15 19:31:56 +0200

    Get certificate for rsin

---
+++ api/src/Controller/ClaimController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route('/claims')
+ */
+class ClaimController
+{
+    /**
+     * @Route("/public_keys/{rsin}")
+     */
+    public function publicKeyAction(CommonGroundService $commonGroundService, string $rsin): Response
+    {
+        $key = $commonGroundService->getResourceList(['component' => 'waar', 'type' => 'metadata/public_keys', 'id' => $rsin])['key'];
+
+        return new Response(
+            $key,
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/x-pem-file']
+        );
+    }
+}
